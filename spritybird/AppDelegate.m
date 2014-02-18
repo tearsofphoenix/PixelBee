2ef0393469986e6cef0a6e1cc441c5001c9f9150
tearsofphoenix <tearsofphoenix@icloud.com>
2014-02-18 15:49:20 +0800

    init.

---
+++ spritybird/AppDelegate.m
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.m
+//  spritybird
+//
+//  Created by Alexis Creuzot on 09/02/2014.
+//  Copyright (c) 2014 Alexis Creuzot. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    // Override point for customization after application launch.
+    return YES;
+}
+
+@end
